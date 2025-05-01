3bc702a93474048939851f5dd24e786f545258b5
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-05-01 20:13:03 -0300

    ✨ Enhance BodyContainer component with props spreading and update Title component styles for improved responsiveness.

--- src/components/bodyContainer.tsx
+++ src/components/bodyContainer.tsx
@@ -1,10 +1,12 @@
-export default function BodyContainer({
-	children,
-	bgColor,
-}: { children: React.ReactNode; bgColor: string }) {
+type BodyContainerProps = {
+	children: React.ReactNode;
+	bgColor: string;
+} & React.HTMLAttributes<HTMLElement>;
+
+export default function BodyContainer({ children, bgColor, ...props }: BodyContainerProps) {
 	return (
-		<section className={`py-20 ${bgColor} text-white`}>
-			<div className="px-4 sm:px-6 lg:px-8">
+		<section className={`py-20 ${bgColor} text-white`} {...props}>
+			<div className="px-4 lg:px-8">
 				<div className="max-w-5xl mx-auto">{children}</div>
 			</div>
 		</section>
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -5,14 +5,14 @@ export default function Title({ title, subtitle }: { title: string; subtitle: st
 
 	return (
 		<div className="text-center mb-16">
-			<h2 className="text-3xl md:text-5xl font-bold mb-6 inline-block relative">
+			<h2 className="text-4xl md:text-5xl font-bold mb-6 inline-block">
 				{firstPart}{" "}
 				<span className="bg-gradient-to-r from-[#06daff] to-[#65e8f9] bg-clip-text text-transparent">
 					{lastWord}
 				</span>
 			</h2>
 			<div className="w-20 h-1 bg-gradient-to-r from-[#01d8ff] to-[#6A3DE8] mx-auto" />
-			<p className="text-lg text-[#c2c4c7] max-w-3xl mx-auto mt-6">{subtitle}</p>
+			<p className="md:text-base text-[12px] text-[#c2c4c7] max-w-3xl mx-auto mt-6">{subtitle}</p>
 		</div>
 	);
 }
